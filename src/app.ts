5c79e6efa6b9bb5d2c5e6269e4357a53044eb921
Rodrigo Tamura <rodrigotamura@hotmail.com>
2020-03-08 01:36:35 -0300

    Users - Listing and creating users

--- src/app.ts
+++ src/app.ts
@@ -2,32 +2,33 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import routes from './routes'
+
 class App {
-    public express: express.Application
+  public express: express.Application;
 
-    constructor () {
-      this.express = express()
+  constructor () {
+    this.express = express()
 
-      this.middlewares()
-      this.routes()
-    }
+    this.middlewares()
+    this.database()
+    this.routes()
+  }
 
-    private middlewares (): void {
-      this.express.use(express.json())
-      this.express.use(cors())
-    }
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
 
-    private database (): void {
-      mongoose.connect('mongodb://localhost:27017/tsnode', {
-        useNewUrlParser: true
-      })
-    }
+  private database (): void {
+    mongoose.connect('mongodb://localhost:27017/tsnode', {
+      useNewUrlParser: true
+    })
+  }
 
-    private routes (): void {
-      this.express.get('/', (req, res) => {
-        return res.send('Hello World')
-      })
-    }
+  private routes (): void {
+    this.express.use(routes)
+  }
 }
 
 export default new App().express
